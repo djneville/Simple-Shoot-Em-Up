0c5f8a41d0d5105b61ea1808d5f3c09ca2bcb259
あああ <lanur622@gmail.com>
2024-11-27 10:43:57 +0900

    particle testing

--- godot/Components/Particles/TrailingParticles.gd
+++ godot/Components/Particles/TrailingParticles.gd
@@ -5,21 +5,22 @@ func _ready() -> void:
     _initialize_particle_material()
     emitting = true
     one_shot = false
-    preprocess = 0.0
 
 func _initialize_particle_material() -> void:
-    self.lifetime = 2.0
+    #self.fixed_fps = 60
+    self.lifetime = 1.0
     self.amount = 10
     self.spread = 0.0
-    self.initial_velocity_max = 5
-    self.initial_velocity_min = 5
-    self.scale_amount_max = 10
-    self.scale_amount_min = 10
-    self.gravity = Vector2(0, 100)
+    self.initial_velocity_max = 1.0
+    self.initial_velocity_min = 1.0
+    self.scale_amount_max = 1
+    self.scale_amount_min = 1
+    self.gravity = Vector2(0, 2)
     self.direction = Vector2(0, 1)
 
     self.color_ramp = Gradient.new()
     color_ramp.add_point(0.0, Color.ROYAL_BLUE)
     color_ramp.add_point(1.0, Color.AZURE)
+    color_ramp.set_interpolation_mode(Gradient.GRADIENT_INTERPOLATE_LINEAR)
 
     self.material = CanvasItemMaterial.new()
--- godot/Tests/Test.gd
+++ godot/Tests/Test.gd
@@ -1,9 +1,11 @@
 extends Node2D
 class_name TestScene
 
-const MOVE_SPEED = 200.0
+const MOVE_SPEED = 10.0
 
 var camera: Camera2D
+var width = 540
+var height = 960
 
 func _ready() -> void:
     center_viewport()
@@ -11,10 +13,11 @@ func _ready() -> void:
 func center_viewport() -> void:
     camera = Camera2D.new()
     camera.position = Vector2.ZERO
+    camera.zoom = Vector2(16.0, 16.0)
     add_child(camera)
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
     var input_vector: Vector2 = Input.get_vector("left", "right", "up", "down")
     for child in get_children():
         if child != camera:
-            child.position += input_vector * MOVE_SPEED * delta
+            child.position += input_vector.normalized() * MOVE_SPEED * delta

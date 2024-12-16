580028160892c24d55a05ff4f8b32cf0354d72ee
あああ <lanur622@gmail.com>
2024-12-16 17:57:05 +0900

    source id issue with glaciers

--- godot/Scenes/Glaciers/GlacialMassDistribution.gd
+++ godot/Scenes/Glaciers/GlacialMassDistribution.gd
@@ -1,6 +1,9 @@
 extends Node2D
 class_name GlacierMassDistribution
 
+#TODO: figure out a good practice for this and the GlacierGen initialization
+const SOURCE_ID = 234
+
 #TODO: ew, just ew...
 var mass_distribution: Array = []
 
@@ -11,7 +14,7 @@ func initialize_mass_distribution(width: int, height: int, glacier_map: TileMapL
         for x in range(width):
             var cell_position = Vector2(x, y)
             mass_distribution[y].append(GlacierCellState.STATE.INTACT)
-            glacier_map.set_cell(cell_position, 0, Vector2i(0, GlacierCellState.STATE.INTACT))
+            glacier_map.set_cell(cell_position, SOURCE_ID, Vector2i(0, GlacierCellState.STATE.INTACT))
             #TODO: at some point figure out if we can just set the cell_state via the atlas coords...?
             var atlas_coords = glacier_map.get_cell_atlas_coords(cell_position) # unused for now
 
@@ -28,4 +31,4 @@ func set_state(cell_position: Vector2, state: GlacierCellState.STATE, glacier_ma
     #TODO: ew again, this wouldnt have to even be checked if the mass_distribution array could be DIRECTLY EMBEDDED in the TileMapLayer...
     if y >= 0 and y < mass_distribution.size() and x >= 0 and x < mass_distribution[y].size():
         mass_distribution[y][x] = state
-        glacier_map.set_cell(cell_position, 0, Vector2i(0, state))
+        glacier_map.set_cell(cell_position, SOURCE_ID, Vector2i(0, state))
